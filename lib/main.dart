c2a058eb41d6274b1b4b0ceee663d103eb5400a0
Aakash Rajbanshi <106716824+aakashx58@users.noreply.github.com>
2024-02-05 19:08:12 +0545

    Update main.dart
    counter screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:demo/bloc/counter/counter_bloc.dart';
+import 'package:demo/ui/counter_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +13,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bloc Demo',
+    return BlocProvider(
+      create: (_) => CounterBloc(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: CounterScreen(),
+      ),
     );
   }
 }
